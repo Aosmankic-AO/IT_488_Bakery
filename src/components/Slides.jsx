c0e0558df953ffc5639cec7fee965346a05617d5
Admir Osmankic <aosmankic@gmail.com>
2023-01-14 20:08:55 -0600

    slideshow/ordering updates

--- src/components/Slides.jsx
+++ src/components/Slides.jsx
@@ -1,23 +1,46 @@
-
-import React, {useState, useEffect} from 'react';
-import data from '../images.json'
+import { Slide } from 'react-slideshow-image';
+import 'react-slideshow-image/dist/styles.css';
+import {pic1, pic2, pic3, pic4, pic5} from '../imgs/images.js'
 
 const Slides = () => {
 
+    const images = [
+        pic1,
+        pic2,
+        pic3,
+        pic4,
+        pic5
+    ];
 
     return (
-      <div className='slides-container'>
-         {data.map(image => (
-          <div key={image.id}>
-            <p>{image.name}</p>
-            <img src={image.path} alt={image.name} />
-          </div>
-        ))}
-      </div>
-      );
-  
-
-
+      <Slide className="slides-container">
+     <div className="each-slide-effect">
+                <div style={{ 'backgroundImage': `url(${images[0]})` }}>
+                    <span>Slide 1</span>
+                </div>
+            </div>
+            <div className="each-slide-effect">
+                <div style={{ 'backgroundImage': `url(${images[1]})` }}>
+                    <span>Slide 2</span>
+                </div>
+            </div>
+            <div className="each-slide-effect">
+                <div style={{ 'backgroundImage': `url(${images[2]})` }}>
+                    <span>Slide 3</span>
+                </div>
+            </div>
+            <div className="each-slide-effect">
+                <div style={{ 'backgroundImage': `url(${images[3]})` }}>
+                    <span>Slide 4</span>
+                </div>
+            </div>
+            <div className="each-slide-effect">
+                <div style={{ 'backgroundImage': `url(${images[4]})` }}>
+                    <span>Slide 5</span>
+                </div>
+            </div>
+  </Slide>
+    )
   }
 
 
